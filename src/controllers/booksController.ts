8265d4dcd908429cefd17a84b85b5fc4da517017
Khuda Dad Nomani <khudadad414@gmail.com>
2024-07-14 20:07:11 +0100

    use json

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -8,23 +8,23 @@ type ControllerFunction = (req: Request, res: Response, next: NextFunction) => v
 
 export const getBooks: ControllerFunction = async (req, res, next) => {
   const books = await getAllBooks()
-  res.status(200).send(books)
+  res.status(200).json(books)
 }
 
 
 export const postBooks: ControllerFunction = async (req, res, next) => {
   const book = await addBook(req.body)
-  res.status(201).send(book)
+  res.status(201).json(book)
 }
 
 export const patchBooksById: ControllerFunction = async (req, res, next) => {
   const bookId = Number.parseInt(req.params["id"])
   const book = await updateBookById(bookId,req.body)
-  res.status(200).send(book)
+  res.status(200).json(book)
 }
 
 export const deleteBooksById: ControllerFunction = async (req, res, next) => {
   const bookId = Number.parseInt(req.params["id"])
   const book = await removeBookById(bookId)
-  res.status(200).send(book)
+  res.status(200).json(book)
 }
