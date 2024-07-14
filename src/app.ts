11385c8c8474669668f32867cc2d1d19260f8a65
Khuda Dad Nomani <khudadad414@gmail.com>
2024-07-14 19:06:35 +0100

    openapi generation

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import swaggerJSDoc from 'swagger-jsdoc';
 import options from './config/openapi.config'
 import * as swaggerUI from 'swagger-ui-express';
 import router from './routes';
+import { generateOpenApiSpec } from './utils/generateOpenAPI';
 
 
 const app: Express = express()
@@ -18,6 +19,7 @@ app.use((error:any,req:any,res:any,next:any)=>{
 
 //Initialize OpenAPI docs
 const spec = swaggerJSDoc(options)
+generateOpenApiSpec(spec)
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(spec))
 
 //Add routes
--- src/utils/generateOpenAPI.ts
+++ src/utils/generateOpenAPI.ts
@@ -1,13 +1,8 @@
 import fs from 'fs';
 import path from 'path';
-import swaggerJSDoc from 'swagger-jsdoc';
-import options from './openapi.config';
 
-const generateOpenApiSpec = () => {
-  const specs = swaggerJSDoc(options);
+export const generateOpenApiSpec = (specs: object) => {
   const outputPath = path.resolve(__dirname, '../../openapi.json');
   fs.writeFileSync(outputPath, JSON.stringify(specs, null, 2));
   return outputPath;
 };
-
-export default generateOpenApiSpec;
